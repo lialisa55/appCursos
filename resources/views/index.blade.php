7cc6263de383b67ee601d0bbe02cb59c984ecfce
unknown <nathalia.mota@etec.sp.gov.br>
2023-09-29 22:57:00 -0300

    Adicionando Icones

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -61,8 +61,32 @@
               href="#0"
               class="collapsed"
               data-bs-toggle="collapse"
-              data-bs-target="#ddmenu_2"
-              aria-controls="ddmenu_2"
+              data-bs-target="#ddmenu_categoria"
+              aria-controls="ddmenu_categoria"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="icon">
+              
+              </span>
+              <span class="text">Categoria</span>
+            </a>
+            <ul id="ddmenu_categoria" class="collapse dropdown-nav">
+              <li>
+                <a href="settings.html"> Adicionar </a>
+              </li>
+              <li>
+                <a href="blank-page.html"> Gerenciar </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item nav-item-has-children">
+            <a
+              href="#0"
+              class="collapsed"
+              data-bs-toggle="collapse"
+              data-bs-target="#ddmenu_curso"
+              aria-controls="ddmenu_curso"
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
@@ -74,24 +98,44 @@
                     d="M13.125 2.29167L16.0417 5.20834H14.1667C13.5913 5.20834 13.125 4.74197 13.125 4.16667V2.29167Z" />
                 </svg>
               </span>
-              <span class="text">Pages</span>
+              <span class="text">Curso</span>
             </a>
-            <ul id="ddmenu_2" class="collapse dropdown-nav">
+            <ul id="ddmenu_curso" class="collapse dropdown-nav">
               <li>
-                <a href="settings.html"> Settings </a>
+                <a href="settings.html"> Cadastrar </a>
               </li>
               <li>
-                <a href="blank-page.html"> Blank Page </a>
+                <a href="blank-page.html"> Gerenciar </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item nav-item-has-children">
+            <a
+              href="#0"
+              class="collapsed"
+              data-bs-toggle="collapse"
+              data-bs-target="#ddmenu_aula"
+              aria-controls="ddmenu_aula"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="icon">
+              </span>
+              <span class="text">Aula</span>
+            </a>
+            <ul id="ddmenu_aula" class="collapse dropdown-nav">
+              <li>
+                <a href="settings.html"> Adicionar </a>
+              </li>
+              <li>
+                <a href="blank-page.html"> Gerenciar </a>
               </li>
             </ul>
           </li>
           <li class="nav-item">
             <a href="invoice.html">
-              <span class="icon">
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M3.33334 3.35442C3.33334 2.4223 4.07954 1.66666 5.00001 1.66666H15C15.9205 1.66666 16.6667 2.4223 16.6667 3.35442V16.8565C16.6667 17.5519 15.8827 17.9489 15.3333 17.5317L13.8333 16.3924C13.537 16.1673 13.1297 16.1673 12.8333 16.3924L10.5 18.1646C10.2037 18.3896 9.79634 18.3896 9.50001 18.1646L7.16668 16.3924C6.87038 16.1673 6.46298 16.1673 6.16668 16.3924L4.66668 17.5317C4.11731 17.9489 3.33334 17.5519 3.33334 16.8565V3.35442ZM4.79168 5.04218C4.79168 5.39173 5.0715 5.6751 5.41668 5.6751H10C10.3452 5.6751 10.625 5.39173 10.625 5.04218C10.625 4.69264 10.3452 4.40927 10 4.40927H5.41668C5.0715 4.40927 4.79168 4.69264 4.79168 5.04218ZM5.41668 7.7848C5.0715 7.7848 4.79168 8.06817 4.79168 8.41774C4.79168 8.76724 5.0715 9.05066 5.41668 9.05066H10C10.3452 9.05066 10.625 8.76724 10.625 8.41774C10.625 8.06817 10.3452 7.7848 10 7.7848H5.41668ZM4.79168 11.7932C4.79168 12.1428 5.0715 12.4262 5.41668 12.4262H10C10.3452 12.4262 10.625 12.1428 10.625 11.7932C10.625 11.4437 10.3452 11.1603 10 11.1603H5.41668C5.0715 11.1603 4.79168 11.4437 4.79168 11.7932ZM13.3333 4.40927C12.9882 4.40927 12.7083 4.69264 12.7083 5.04218C12.7083 5.39173 12.9882 5.6751 13.3333 5.6751H14.5833C14.9285 5.6751 15.2083 5.39173 15.2083 5.04218C15.2083 4.69264 14.9285 4.40927 14.5833 4.40927H13.3333ZM12.7083 8.41774C12.7083 8.76724 12.9882 9.05066 13.3333 9.05066H14.5833C14.9285 9.05066 15.2083 8.76724 15.2083 8.41774C15.2083 8.06817 14.9285 7.7848 14.5833 7.7848H13.3333C12.9882 7.7848 12.7083 8.06817 12.7083 8.41774ZM13.3333 11.1603C12.9882 11.1603 12.7083 11.4437 12.7083 11.7932C12.7083 12.1428 12.9882 12.4262 13.3333 12.4262H14.5833C14.9285 12.4262 15.2083 12.1428 15.2083 11.7932C15.2083 11.4437 14.9285 11.1603 14.5833 11.1603H13.3333Z" />
-                </svg>
+              <span class="icon">                
+              <i class="lni lni-blackboard"></i>
               </span>
               <span class="text">Invoice</span>
             </a>
@@ -101,8 +145,8 @@
               href="#0"
               class="collapsed"
               data-bs-toggle="collapse"
-              data-bs-target="#ddmenu_3"
-              aria-controls="ddmenu_3"
+              data-bs-target="#ddmenu_d6"
+              aria-controls="ddmenu_d6"
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
@@ -116,7 +160,7 @@
               </span>
               <span class="text">Auth</span>
             </a>
-            <ul id="ddmenu_3" class="collapse dropdown-nav">
+            <ul id="ddmenu_6" class="collapse dropdown-nav">
               <li>
                 <a href="signin.html"> Sign In </a>
               </li>
