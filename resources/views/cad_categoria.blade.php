ebb890d9ac3ee31602a69f20d4fbedb113415494
unknown <nathalia.mota@etec.sp.gov.br>
2023-10-02 22:58:56 -0300

    excluindo front não utilizado

---
+++ resources/views/cad_categoria.blade.php
@@ -0,0 +1,31 @@
+@include('padrao')
+@section('conteudo')
+<!-- ======= select style start ======= -->
+<div class="card-style mb-30">
+                  <h6 class="mb-25">Selects</h6>
+                  <div class="select-style-1">
+                    <label>Category</label>
+                    <div class="select-position">
+                      <select>
+                        <option value="">Select category</option>
+                        <option value="">Category one</option>
+                        <option value="">Category two</option>
+                        <option value="">Category three</option>
+                      </select>
+                    </div>
+                  </div>
+                  <!-- end select -->
+                  <div class="select-style-2">
+                    <div class="select-position">
+                      <select>
+                        <option value="">Select category</option>
+                        <option value="">Category one</option>
+                        <option value="">Category two</option>
+                        <option value="">Category three</option>
+                      </select>
+                    </div>
+                  </div>
+                  <!-- end select -->
+                </div>
+                <!-- end card -->
+                <!-- ======= select style end ======= -->
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/cadastrocategoria', function () {
+    return view('cad_categoria');
+});
+
